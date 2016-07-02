3ed6a76978727350cc9158e03ab8dad0b5c12473
caivenlf <Zero.Lee@vincent.local>
2016-07-03 01:02:16 +0800

    [Add: ] __submit branch 0.0.1
    [Add: ] __submit branch 0.0.1

---
+++ PieceStore/CodingSupport.swift
@@ -0,0 +1,39 @@
+//
+//  CodingSupport.swift
+//  CodingSupport
+//
+//  Created by Zero on 16/7/2.
+//  Copyright © 2016年 Zero. All rights reserved.
+//
+
+import UIKit
+
+class CodingSupport: NSObject, NSCoding {
+    func encodeWithCoder(aCoder: NSCoder) {
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(self.classForCoder, &count)
+        for i in 0..<Int(count) {
+            let ivar = ivars[i]
+            let key = NSString(CString: ivar_getName(ivar), encoding: NSUTF8StringEncoding) as! String
+            let value = self.valueForKey(key)
+            aCoder.encodeObject(value, forKey: key)
+        }
+        free(ivars)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(self.classForCoder, &count)
+        for i in 0..<Int(count) {
+            let ivar = ivars[i]
+            let key = NSString(CString: ivar_getName(ivar), encoding: NSUTF8StringEncoding) as! String
+            let value = aDecoder.decodeObjectForKey(key)
+            self.setValue(value, forKey: key)
+        }
+    }
+    
+    override init() {
+        super.init()
+    }
+}
