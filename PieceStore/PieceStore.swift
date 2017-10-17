75363d02706bf73c0faa9245309838caa9ab4dd8
李锋 <lifeng@oudmon.com>
2017-10-17 17:22:23 +0800

    Support swift 4

--- PieceStore/PieceStore.swift
+++ PieceStore/PieceStore.swift
@@ -24,12 +24,46 @@ public class PieceStore: NSObject {
         return defaultUserName
     }
     
+    /*
+     @swfit4
+     */
+    public class func save<T: Codable>(obj: T) {
+        
+        let encoder = JSONEncoder()
+        let encoded = try? encoder.encode(obj)
+        
+        UserDefaults.standard.set(encoded, forKey: String(format: "user_id_%@_type_%@", PieceStore.userId(), String(describing: type(of: obj))))
+        UserDefaults.standard.synchronize()
+    }
+    
+    /*
+     @swift4
+     */
+    public class func get<T: Codable>(type: T.Type) -> T? {
+        
+        let infoData = UserDefaults.standard.object(forKey: String(format: "user_id_%@_type_%@", userId(), String(describing: type)))
+        
+        guard let info = infoData as? Data else { return nil }
+        
+        let decoder = JSONDecoder()
+        if let decoded = try? decoder.decode(T.self, from: info) {
+            return decoded
+        }
+        return nil
+    }
+    
+    /*
+     @swift3
+     */
     public class func saveInfo(obj: NSObject) {
         let infoData = NSKeyedArchiver.archivedData(withRootObject: obj)
         UserDefaults.standard.set(infoData, forKey: String(format: "user_id_%@_type_%@", PieceStore.userId(), String(describing: type(of: obj))))
         UserDefaults.standard.synchronize()
     }
     
+    /*
+     @swift3
+     */
     public class func getInfo<T: NSObject>(type: T.Type) -> T {
         let infoData = UserDefaults.standard.object(forKey: String(format: "user_id_%@_type_%@", userId(), String(describing: type))) as? NSData
         if let _infoData = infoData {
@@ -39,12 +73,18 @@ public class PieceStore: NSObject {
         return type.init()
     }
     
+    /*
+     @swift3
+     */
     public class func update(modelType: NSObject.Type, value:AnyObject, key: String) {
         let info = getInfo(type: modelType)
         info.setValue(value, forKey: key)
         saveInfo(obj: info)
     }
     
+    /*
+     @swift3
+     */
     public class func get(modelType: NSObject.Type, key: String) -> AnyObject? {
         if let value = getInfo(type: modelType).value(forKey: key) {
             return value as AnyObject?
